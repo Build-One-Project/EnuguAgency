e9a8f9fbe3b57ce318bad2e3af4bee7bf5f16244
nmasi322 <edehdivine042@gmail.com>
2021-05-31 23:38:57 +0100

    Done with the FAQ page!

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+//When the user clicks on the button, scroll to the top of the document
 mybutton.addEventListener('click', () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
