9d2c7e52a70bc678363f6db1d70996f16c96e076
nmasi322 <edehdivine042@gmail.com>
2021-07-18 18:09:10 +0100

    Login form and dashboard

--- contact.js
+++ contact.js
@@ -1,29 +1,3 @@
-// //Get the button:
-// mybutton = document.getElementById("myBtn");
-
-// // When the user scrolls down 50px from the top of the document, show the button
-// window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-//     mybutton.style.display = "block";
-//   } else {
-//     mybutton.style.display = "none";
-//   }
-// }
-
-// // When the user clicks on the button, scroll to the top of the document
-// mybutton.addEventListener('click', () => {
-//   document.body.scrollTop = 0;
-//   document.documentElement.scrollTop = 0;
-// })
-
-// function googleTranslateElementInit() { 
-//   new google.translate.TranslateElement(
-//       {pageLanguage: 'en'}, 
-//       'google_translate'
-// )}
-
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -42,3 +16,49 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+// Login
+function Login(){
+    var a = new Array();
+    up1 = new Object();
+    up2 = new Object();
+
+    up1 = {
+        name: "edehdivine042@gmail.com",
+        password: btoa("nmasi2008")
+    };
+
+    up2 = {
+        name: "edehcalister@gmail.com",
+        password: btoa("cally1967")
+    };
+
+    var username = document.getElementById("uname").value;
+    var password = document.getElementById("psw").value;
+
+    localStorage.setItem("all_users", JSON.stringify(a));
+
+    a = JSON.parse((localStorage.getItem("all_users")));
+    a.push({name: username, password: password});
+    localStorage.setItem("name", JSON.stringify(a));
+
+    for (var i = 0; i < a.length; i++){
+        var li = document.createElement("li");
+        li.innerHTML = a[i]["name"];
+        document.getElementById("listuser").appendChild(li);
+    }
+}
+// function Login(){
+//     const hash = Object.fromEntries(a.map(e => [e.name, e.password]))
+//     var username = document.getElementById("uname").value;
+//     var password = document.getElementById("psw").value;
+//     for(let key of hash){
+//         if (key[0] === username && key[1] === atob(password)){
+//             alert("Login Successful");
+//         }
+//         else{
+//             alert("Login Failed")
+//         }
+//     }
+//   }
+  
---
+++ login.js
@@ -0,0 +1,44 @@
+//Loader 
+const loaderContainer = document.querySelector(".loader")
+
+window.addEventListener("load", () => {
+  loaderContainer.classList.add("hide")
+})
+
+// Menu
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+//Signup/Signin
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden");
+    });
+
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("form--hidden");
+        createAccountForm.classList.add("form--hidden");
+    });
+});
