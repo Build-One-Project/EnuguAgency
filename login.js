ebb772215c8e2418195f0eefd49fe2fcf91ee9a9
nmasi322 <edehdivine042@gmail.com>
2021-07-21 00:38:26 +0100

    This website is finally done!

--- login.js
+++ login.js
@@ -31,13 +31,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const createAccountForm = document.querySelector("#createAccount");
 
     document.querySelector("#linkCreateAccount").addEventListener("click", e => {
-        e.preventDefault();
+        // e.preventDefault();
         loginForm.classList.add("form--hidden");
         createAccountForm.classList.remove("form--hidden");
     });
 
     document.querySelector("#linkLogin").addEventListener("click", e => {
-        e.preventDefault();
+        // e.preventDefault();
         loginForm.classList.remove("form--hidden");
         createAccountForm.classList.add("form--hidden");
     });
